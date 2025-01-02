b7f466cbbe53adb89ac80b39a394ec94abfd4407
kaashyapan <sunder.narayanaswamy@gmail.com>
2025-01-02 18:42:03 +0530

    Add TLS-ALPN-01 sample
    https://www.haproxy.com/blog/haproxy-and-let-s-encrypt
    
    https://github.com/acmesh-official/acme.sh/wiki/TLS-ALPN-without-downtime

---
+++ haproxy-acme-tlsalpn01/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+docker run -it --rm --name haproxy-acme-tlsalpn01 \
+    -v "$SCRIPT_DIR/volume/acme:/var/lib/acme:rw" \
+    -e "HAPROXY_HTTP_PORT=80" \
+    -e "HAPROXY_HTTPS_PORT=443" \
+    -e "HAPROXY_HTTPS_REASSIGN_PORT=8443" \
+    -e "ACME_DEBUG=0" \
+    -e "ACME_UPGRADE=1" \
+    -e "ACME_CROM=1" \
+    -e "ACME_MAIL=mail@domain.com" \
+    -e "ACME_SERVER=letsencrypt_test" \
+    -e "ACME_DOMAIN=domain.com" \
+    -e "ACME_KEYLENGTH=ec-256" \
+    -e "ACME_TLSALPN_PORT=10443" \
+    -e "SERVER_ADDRESS=whoami" \
+    -e "SERVER_PORT=80" \
+    -e "SERVER_DIRECTIVES=" \
+    -p 80:80 \
+    -p 443:443 \
+    --sysctl net.ipv4.ip_unprivileged_port_start=0 \
+    flobernd/haproxy-acme-tlsalpn01
